4baadf403c595fa2231dcba832239412e943645f
geet-h17 <mailmeatgeet@gmail.com>
2023-06-15 23:42:56 +0530

    sprint automation

---
+++ sprintdetails/1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+##########################################
+
+Script to automate sprint files
+
+Author : geet
+Version: v1
+Date: 12/june
+##########################################
+
